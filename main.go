fc9481ad29d3ec1a27300b93eeb6b4eda2bd7391
Brian Haack <haack79@hotmail.com>
2024-08-13 13:23:37 -0700

    added todos functionality

--- main.go
+++ main.go
@@ -2,9 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	name := "World!"
 	fmt.Printf("hello, %s!\n", name)
+	// initialize db
+	InitDatabase()
+	defer DB.Close() // close defers closing once the main function is done executing.
+
+	e := gin.Default()
+	e.LoadHTMLGlob("templates/*")
+	e.GET("/", func(c *gin.Context) {
+		todos := ReadToDoList()
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"todos": todos,
+		})
+		// c.JSON(200, gin.H{
+		// 	"name": "Awesome",
+		// })
+	})
+	e.POST("/todos", func(c *gin.Context) {
+		title := c.PostForm("title")
+		status := c.PostForm("status")
+		id, _ := CreateToDo(title, status)
+		c.HTML(http.StatusOK, "task.html", gin.H{
+			"title":  title,
+			"status": status,
+			"id":     id,
+		})
+	})
+	e.DELETE("/todos/:id", func(c *gin.Context) {
+		param := c.Param("id")
+		id, _ := strconv.ParseInt(param, 10, 64)
+		DeleteToDo(id)
+	})
+	e.Run(":8080")
 }
